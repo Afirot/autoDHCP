f2a116731637fa93ff4e447b8b3045eee23266e7
Afirot <153087902+Afirot@users.noreply.github.com>
2024-10-30 12:49:40 +0100

    Update autoDHCP.py

--- autoDHCP.py
+++ autoDHCP.py
@@ -2,7 +2,7 @@
 #Descripcion: Este script sirve para configurar automaticamente los hosts en DHCPD en linux mediante una lista de direcciones mac e ip en formato txt.
 import sys
 import os
-
+import re
 
 def leer(archivo): #Esta funcion leera el archivo que se haya transmitido como argumento y almacenara su contenido en una lista, siendo dividido el texto por los saltos de linea
     global lista
@@ -51,21 +51,31 @@ def cre_list(lista): #Esta funcion tomara la lista creada en la funcion leer() y
         mac.append(listaFinal[1])
         tipo.append(listaFinal[2])
         
-def repre(grupo, lis1, lis2, lis4): # Esta funcion sirve para escribir todo en el archibo de configuracion
+def repre(grupo, lis1, lis2, lis3, exis): # Esta funcion sirve para escribir todo en el archibo de configuracion
 
     with open(RUT_CONF, "a") as file:
-        file.write(f"group {grupo}{{\n")
+        file.write(f"group {grupo} {{\n")
         for i in range(0, len(lis1)):
-            file.write(f"\t#HOST {lis1[i]}\n")
-            file.write(f"\t{lis1[i]} {{ \n")
-            file.write(f"\thardware ethernet {lis2[i]} ; \n")
-            file.write(f"\tfixed-address {lis4[i]};\n")
-            file.write("\t}\n\n")
+            if lis2[i] not in exis:
+                file.write(f"\t#HOST {lis1[i]}\n")
+                file.write(f"\t{lis1[i]} {{\n")
+                file.write(f"\thardware ethernet {lis2[i]} ; \n")
+                file.write(f"\tfixed-address {lis3[i]};\n")
+                file.write("\t}\n\n")
+            else:
+                print(f"La direccion mac {lis2[i]} ya existe")
         file.write("}\n")
-                
 
+def existen(rut_conf): #Esta funcion extrae una lista de las macs que esisten en el archivo de configuracion
 
-def asig_ip(dir_red, minimo, maximo): #Esta funcion crea las ips en el rango especificado, en caso de ser una ip </24 debera añadirse otro for
+    with open(rut_conf, "r") as file:
+        #for l in file:
+        cadena = file.read()
+        macs = (re.findall(r"(?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2})", cadena))
+        
+    return macs
+
+def asig_ip(dir_red, minimo, maximo): #Asignar ip
     ip_list = []
     for i in range (minimo, maximo):
         if i < 255:
@@ -79,7 +89,7 @@ def main(): # Esta funcion llama al resto
     global fichero, lista, dispositivo, mac, tipo, wireless, desktop, server, RUT_CONF
     
     RUT_CONF = "/etc/dhcp/dhcpd.conf"
-    fichero = sys.argv[1]
+    fichero = "IP_Fijas_17102024.txt"
     lista = []
     dispositivo = []
     mac = []
@@ -92,23 +102,25 @@ def main(): # Esta funcion llama al resto
 
 
     if compr_arg() and compr_fichero(): #Si los dos comprobadores son True, se inicia main.
+        existentes = existen(RUT_CONF)
         print("Iniciando script ...")
         leer(fichero)
         sep_list(lista)
 
         cre_list(wireless)
         ip_wireless = asig_ip(mascara, 12, 40)
-        repre("wireless", dispositivo, mac, ip_wireless)
+        repre("Wireless",dispositivo, mac, ip_wireless, existentes)
 
         cre_list(desktop)
         ip_desktop = asig_ip(mascara, 50, 80)
-        repre("desktop", dispositivo, mac, ip_desktop)
+        repre("Desktop",dispositivo, mac, ip_desktop, existentes)
 
         cre_list(server)
         ip_server = asig_ip(mascara, 230, 240)
-        repre("server", dispositivo, mac, ip_server)
+        repre("Server",dispositivo, mac, ip_server, existentes)
 
         print("Configuracion finalizada")
     
     
 main()
+
